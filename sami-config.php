c1bc2d96e49f18354982b5500478bb511e55fea9
Max Loeb <shmax@shmax.com>
2017-05-28 16:14:01 -0700

    tune versions

--- sami-config.php
+++ sami-config.php
@@ -6,12 +6,10 @@ use Symfony\Component\Finder\Finder;
 
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
 $versions = GitVersionCollection::create('lib')
-    ->addFromTags('v1.0.*')
-    ->add('master', 'master branch')
+    ->add('generate-docs', 'docs branch')
 ;
 
 return new Sami('lib', [
-    'theme' => 'symfony',
     'versions' => $versions,
     'build_dir' => 'docs/build/%version%',
     'cache_dir' => 'docs/cache/%version%'
