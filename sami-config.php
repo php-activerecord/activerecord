c37cbba4c1cda5ff2880bc707c871e51741dbff2
Max Loeb <shmax@shmax.com>
2017-05-28 16:06:02 -0700

    wip

--- sami-config.php
+++ sami-config.php
@@ -1,5 +1,18 @@
 <?php
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
 
-return new Sami\Sami('lib', [
-    'build_dir' => 'docs/sami/docs'
+// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
+$versions = GitVersionCollection::create('lib')
+    ->addFromTags('v1.0.*')
+    ->add('master', 'master branch')
+;
+
+return new Sami('lib', [
+    'theme' => 'symfony',
+    'versions' => $versions,
+    'build_dir' => 'docs/build/%version%',
+    'cache_dir' => 'docs/cache/%version%'
 ]);
