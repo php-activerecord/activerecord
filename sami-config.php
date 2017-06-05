ae5789cec6301efb2a2e219e5cfd53541d52b699
Max Loeb <shmax@shmax.com>
2017-06-04 20:44:02 -0700

    simplify docs structure

--- sami-config.php
+++ sami-config.php
@@ -1,33 +1,32 @@
 <?php
 
 use Sami\Sami;
-use Sami\Version\GitVersionCollection;
+//use Sami\Version\GitVersionCollection;
 
-$versions = GitVersionCollection::create('lib')
-    ->addFromTags('v1.0')
-    ->add('master', 'master branch')
-;
-
-return new Sami('lib', [
-    'title' => 'php-activerecord API',
-    'theme' => 'php-ar',
-    'versions' => $versions,
-    'build_dir' => 'build/docs/api/%version%',
-    'cache_dir' => 'build/docs/api/cache/%version%',
-    'template_dirs' => [
-        'docs/sami/themes'
-    ]
-]);
-
-// for local dev
+//$versions = GitVersionCollection::create('lib')
+//    ->addFromTags('v1.0')
+//    ->add('master', 'master branch')
+//;
 
 //return new Sami('lib', [
 //    'title' => 'php-activerecord API',
-//    'usage_docs_title' => 'Usage',
 //    'theme' => 'php-ar',
-//    'build_dir' => 'build/api',
-//    'cache_dir' => 'build/api/cache',
+//    'versions' => $versions,
+//    'build_dir' => 'build/docs/api/%version%',
+//    'cache_dir' => 'build/docs/api/cache/%version%',
 //    'template_dirs' => [
 //        'docs/sami/themes'
 //    ]
 //]);
+
+// for local dev
+
+return new Sami('lib', [
+    'title' => 'php-activerecord API',
+    'theme' => 'php-ar',
+    'build_dir' => 'build/docs/api',
+    'cache_dir' => 'build/docs/api/cache',
+    'template_dirs' => [
+        'docs/sami/themes'
+    ]
+]);
