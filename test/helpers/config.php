060c8008dec49b5d597a8cc80bfa7b4c6f9b7d51
Max Loeb <shmax@shmax.com>
2023-08-20 11:38:18 -0700

    update phpunit.dist.xml, fix require

--- test/helpers/config.php
+++ test/helpers/config.php
@@ -17,7 +17,7 @@
  **/
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-require_once 'vendor/phpunit/phpunit/src/Framework/TestCase.php';
+require_once __DIR__ . '/../../vendor/phpunit/phpunit/src/Framework/TestCase.php';
 require_once 'SnakeCase_PHPUnit_Framework_TestCase.php';
 
 
