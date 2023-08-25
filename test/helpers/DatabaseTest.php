51832863bbfaf8ea98da9fb9a31dd83846ed1162
Max Loeb <shmax@shmax.com>
2023-08-25 08:55:39 -0700

    fix warnings

--- test/helpers/DatabaseTest.php
+++ test/helpers/DatabaseTest.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/DatabaseLoader.php';
 class DatabaseTest extends SnakeCase_PHPUnit_Framework_TestCase
 {
     protected $conn;
+    protected $original_date_class;
     public static $log = false;
     public static $db;
 
