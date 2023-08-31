57ade01afc830586b283803b13b96c48c3384085
Max Loeb <shmax@shmax.com>
2023-08-31 16:51:06 -0700

    stanning CsvSerializer

--- lib/Serialize/CsvSerializer.php
+++ lib/Serialize/CsvSerializer.php
@@ -5,12 +5,11 @@ namespace ActiveRecord\Serialize;
 /**
  * CSV serializer.
  *
- * @package ActiveRecord
  */
 class CsvSerializer extends Serialization
 {
-    public static $delimiter = ',';
-    public static $enclosure = '"';
+    public static string $delimiter = ',';
+    public static string $enclosure = '"';
 
     public function to_s(): string
     {
@@ -21,17 +20,21 @@ class CsvSerializer extends Serialization
         return $this->row();
     }
 
-    private function header()
+    private function header(): string
     {
         return $this->to_csv(array_keys($this->to_a()));
     }
 
-    private function row()
+    private function row(): string
     {
         return $this->to_csv($this->to_a());
     }
 
-    private function to_csv($arr)
+    /**
+     * @param array<mixed> $arr
+     * @return string
+     */
+    private function to_csv(array $arr): string
     {
         $outstream = fopen('php://temp', 'w');
         fputcsv($outstream, $arr, self::$delimiter, self::$enclosure);
