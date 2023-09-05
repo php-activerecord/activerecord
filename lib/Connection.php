9a9c79d6fd630d67d44defbd9b5fd4f8411045c7
Max Loeb <shmax@shmax.com>
2023-09-05 16:43:49 -0700

    remove native database types thing (#61)

--- lib/Connection.php
+++ lib/Connection.php
@@ -563,15 +563,6 @@ abstract class Connection
      */
     abstract public function set_encoding(string $charset): void;
 
-    /*
-     * Returns an array mapping of native database types
-     */
-
-    /**
-     * @return array<string, string|array<string, mixed>>
-     */
-    abstract public function native_database_types(): array;
-
     /**
      * Specifies whether adapter can use LIMIT/ORDER clauses with DELETE & UPDATE operations
      *
