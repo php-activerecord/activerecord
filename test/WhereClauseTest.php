2de176fba926794283bb63fad2fb92a61fa9b891
Max Loeb <shmax@shmax.com>
2023-09-17 20:17:35 -0700

    increase coverage

--- test/WhereClauseTest.php
+++ test/WhereClauseTest.php
@@ -86,116 +86,123 @@ class WhereClauseTest extends TestCase
         $this->assertEquals([null], $a->values());
     }
 
-    public function testZeroVariable()
+    public function testZeroVariable(): void
     {
         $a = new WhereClause('name=?', [0]);
         $this->assertEquals('name=?', $a->to_s($this->connection));
         $this->assertEquals([0], $a->values());
     }
 
-    public function testEmptyArrayVariable()
+    public function testEmptyArrayVariable(): void
     {
         $a = new WhereClause('id IN(?)', [[]]);
         $this->assertEquals('id IN(?)', $a->to_s($this->connection));
         $this->assertEquals([[]], $a->values());
     }
 
-    public function testIgnoreInvalidParameterMarker()
+    public function testEmptyArrayVariableSubstitute(): void
+    {
+        $a = new WhereClause('id IN(?)', [[]]);
+        $this->assertEquals('id IN(NULL)', $a->to_s($this->connection, substitute: true));
+        $this->assertEquals([[]], $a->values());
+    }
+
+    public function testIgnoreInvalidParameterMarker(): void
     {
         $a = new WhereClause("question='Do you love backslashes?' and id in(?)", [[1, 2]]);
         $this->assertEquals("question='Do you love backslashes?' and id in(?,?)", $a->to_s($this->connection));
     }
 
-    public function testIgnoreParameterMarkerWithEscapedQuote()
+    public function testIgnoreParameterMarkerWithEscapedQuote(): void
     {
         $a = new WhereClause("question='Do you love''s backslashes?' and id in(?)", [[1, 2]]);
         $this->assertEquals("question='Do you love''s backslashes?' and id in(?,?)", $a->to_s($this->connection));
     }
 
-    public function testIgnoreParameterMarkerWithBackspaceEscapedQuote()
+    public function testIgnoreParameterMarkerWithBackspaceEscapedQuote(): void
     {
         $a = new WhereClause("question='Do you love\\'s backslashes?' and id in(?)", [[1, 2]]);
         $this->assertEquals("question='Do you love\\'s backslashes?' and id in(?,?)", $a->to_s($this->connection));
     }
 
-    public function testSubstituteOnString()
+    public function testSubstituteOnString(): void
     {
         $a = new WhereClause('name=? and id=?', ['Tito', 1]);
         $this->assertEquals("name='Tito' and id=1", $a->to_s($this->connection, substitute: true));
     }
 
-    public function testSubstituteOnHash()
+    public function testSubstituteOnHash(): void
     {
         $a = new WhereClause(['name' => 'Tito', 'id'=> 1]);
         $this->assertEquals("`name` = 'Tito' AND `id` = 1", $a->to_s($this->connection, substitute: true));
     }
 
-    public function testSubstituteQuotesScalarsButNotOthers()
+    public function testSubstituteQuotesScalarsButNotOthers(): void
     {
         $a = new WhereClause('id in(?)', [[1, '2', 3.5]]);
         $this->assertEquals("id in(1,'2',3.5)", $a->to_s($this->connection, substitute: true));
     }
 
-    public function testSubstituteWhereValueHasQuestionMark()
+    public function testSubstituteWhereValueHasQuestionMark(): void
     {
         $a = new WhereClause('name=? and id=?', ['??????', 1]);
         $this->assertEquals("name='??????' and id=1", $a->to_s($this->connection, substitute: true));
     }
 
-    public function testSubstituteArrayValue()
+    public function testSubstituteArrayValue(): void
     {
         $a = new WhereClause('id in(?)', [[1, 2]]);
         $this->assertEquals('id in(1,2)', $a->to_s($this->connection, substitute: true));
     }
 
-    public function testSubstituteEscapeQuotesWithConnectionsEscapeMethod()
+    public function testSubstituteEscapeQuotesWithConnectionsEscapeMethod(): void
     {
         $a = new WhereClause('name=?', ["Tito's Guild"]);
         $escaped = $this->connection->escape("Tito's Guild");
         $this->assertEquals("name=$escaped", $a->to_s($this->connection, substitute: true));
     }
 
-    public function testBind()
+    public function testBind(): void
     {
         $a = new WhereClause('name=? and id=?', ['Tito']);
         $a->bind(2, 1);
         $this->assertEquals(['Tito', 1], $a->values());
     }
 
-    public function testBindOverwriteExisting()
+    public function testBindOverwriteExisting(): void
     {
         $a = new WhereClause('name=? and id=?', ['Tito', 1]);
         $a->bind(2, 99);
         $this->assertEquals(['Tito', 99], $a->values());
     }
 
-    public function testBindInvalidParameterNumber()
+    public function testBindInvalidParameterNumber(): void
     {
         $this->expectException(ExpressionsException::class);
         $a = new WhereClause('name=?');
         $a->bind(0, 99);
     }
 
-    public function testSubstituteUsingAlternateValues()
+    public function testSubstituteUsingAlternateValues(): void
     {
         $a = new WhereClause('name=?', ['Tito']);
         $this->assertEquals("name='Tito'", $a->to_s($this->connection, substitute: true));
         $this->assertEquals("name='Hocus'", $a->to_s($this->connection, substitute: true, values: ['Hocus']));
     }
 
-    public function testNullValue()
+    public function testNullValue(): void
     {
         $a = new WhereClause('name=?', [null]);
         $this->assertEquals('name=NULL', $a->to_s($this->connection, substitute: true));
     }
 
-    public function testHashWithDefaultGlue()
+    public function testHashWithDefaultGlue(): void
     {
         $a = new WhereClause(['id' => 1, 'name' => 'Tito']);
         $this->assertEquals('`id` = ? AND `name` = ?', $a->to_s($this->connection));
     }
 
-    public function testHashWithGlue()
+    public function testHashWithGlue(): void
     {
         $a = new WhereClause([
             'id' => 1,
@@ -204,7 +211,7 @@ class WhereClauseTest extends TestCase
         $this->assertEquals('`id` = ?, `name` = ?', $a->to_s($this->connection, glue: ', '));
     }
 
-    public function testHashWithArray()
+    public function testHashWithArray(): void
     {
         $a = new WhereClause([
             'id' => 1,
